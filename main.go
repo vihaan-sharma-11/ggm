cbebc6adcaaf964e2f292c98a31da31d915331f8
vihaan-sharma-11 <vihaansharmakushal@gmail.com>
2025-06-30 12:12:29 +0530

    add

--- main.go
+++ main.go
@@ -1,33 +1,46 @@
-// package main
-
-// import "fmt"
-
-// func main ()  {
-// 	fmt.Pritln("hello world")
-// 	app := fiber.New()
-// 	app.Get("/abc",func(c *fiber.ctx) error{
-// 			return c. sendstarting("hello world")
-
-//		})
-//		app.Listen(":3000")
-//	}
-
 package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func main(){
-	fmt.Printf("hello")
+func main() {
+	fmt.Println("Hello, World!")
 
-	     app:=fiber.New()
-		app.Get("/abc",func(c *fiber.Ctx) error{
-			return c. SendString("hello world")
+	app := fiber.New()
 
+	app.Get("/abc", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World! 1234")
+	})
+
+	app.Post("/sum", AddOperation)
+
+	log.Fatal(app.Listen(":3000"))
+}
+
+func AddOperation(c *fiber.Ctx) error {
+	var request AddOperationRequest
+
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid request body",
 		})
-		app.Listen(":3000")
-	
-} 
+	}
+
+	result := request.Num1 + request.Num2
+	response := AddOperationResponse{Result: result}
+
+	return c.JSON(response)
+}
+
+type AddOperationRequest struct {
+	Num1 int `json:"num1"`
+	Num2 int `json:"num2"`
+}
+
+type AddOperationResponse struct {
+	Result int `json:"result"`
+}
